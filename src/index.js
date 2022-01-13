c94b4b71ae6f015a50316633b4a7f84fecdc3b5b
Andres Vargas <andreselias.vargas@gmail.com>
2022-01-13 08:22:46 -0600

    Update file
    Tabs functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
 
 //Tooltip 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -12,3 +13,8 @@ dropdowns.forEach(dropdown => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
+
+// Tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
