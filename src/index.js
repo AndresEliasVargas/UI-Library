fe287942fd73aa7483652f241cf80ad83157ccac
Andres Vargas <andreselias.vargas@gmail.com>
2022-01-13 09:09:47 -0600

    Update file
    Snackbar functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
 import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 
 //Tooltip 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -18,3 +19,12 @@ dropdowns.forEach(dropdown => {
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
 
+// Snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+let message = 'You clicked me :)';
+button.addEventListener('click', () => {
+  snackbar.show(message);
+});
